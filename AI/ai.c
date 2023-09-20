6123613e5f644f5beb1eb4ead826531fd205172e
Mohammed Mahadi Hossain <132497252+m13hmahadi250@users.noreply.github.com>
2023-09-21 00:07:33 +0600

    init

---
+++ AI/ai.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int   a, b, c, hi;
+
+    printf("What is your name ? \n:- ");
+
+    char name[50];
+
+    scanf("%s", name);
+
+    printf("Hello, %s, How can I help you ?\n", name);
+
+
+    scanf("%d", &a);
+
+
+    if( a == hi ){
+
+        printf(" Hi Dear, All good\n");
+    }
+    else{
+
+       printf("Hi Dear, All good\n");
+    }
+
+    scanf("%d", &b);
+
+    if( a == hi ){
+
+        printf(" \n");
+    }
+    else{
+
+       printf("I have no feelings\n");
+    }
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
